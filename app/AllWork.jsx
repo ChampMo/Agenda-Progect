bd7e8ae9a6a5ca01a13f41628f16866c97fb134d
Navapon <97166582+peetnavapon@users.noreply.github.com>
2024-04-08 23:22:25 +0700

    allwork2

--- app/AllWork.jsx
+++ app/AllWork.jsx
@@ -1,6 +1,7 @@
 import './AllWork.css'
 import vectorlogin from '../public/images/vector-login.png';
 import {Navbar} from '../components/Navbar'
+import img1 from '../public/images/logingrp.png';
 
 function AllWork(){
     return(
@@ -17,22 +18,24 @@ function AllWork(){
                         <div className='mail'><h4>Workspaces for ...</h4></div>
                         <div className='subwork'>
                             <p>Something Workspace</p>
-                            <button>Launch</button>
+                            <button>LAUNCH</button>
                         </div>
                         <div className='subwork'>
                             <p>Something Workspace</p>
-                            <button>Launch</button>
+                            <button>LAUNCH</button>
                         </div>
                         <div className='subwork'>
                             <p>Something Workspace</p>
-                            <button>Launch</button>
+                            <button>LAUNCH</button>
                         </div>
                     </div>
                     <div className='or'>
                         OR
                     </div>
                     <div className='create'>
-                        
+                        <div className='first'><img src={img1} alt="" /></div>
+                        <div className='mid'>Create a new Workspace</div>
+                        <div className='last'><button>Create!</button></div>
                     </div>
                 </div>
             </div>
