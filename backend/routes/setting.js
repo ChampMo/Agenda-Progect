21c8610d87967884f8bb0998dd0d96843ccfe3a0
Navapon <97166582+peetnavapon@users.noreply.github.com>
2024-06-06 13:53:25 +0700

    addrole backend

--- backend/routes/setting.js
+++ backend/routes/setting.js
@@ -6,27 +6,27 @@ const router = express.Router();
 
 
 
-router.get("/api/profileInfo/", async (req, res) => {
-    try {
-        console.log(req.session.userId, "req.session.userId")
-        const result = await User.findOne({user_id:req.session.userId});
+// router.get("/api/profileInfo/", async (req, res) => {
+//     try {
+//         console.log(req.session.userId, "req.session.userId")
+//         const result = await User.findOne({user_id:req.session.userId});
 
-        return res.json({
-            userInfo:result,
-            massage: "Send userInfo Successfully!",
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
+//         return res.json({
+//             userInfo:result,
+//             massage: "Send userInfo Successfully!",
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// });
 
-router.post("/api/addrole/", async (res,req)=>{
+router.post("/api/addrole/", async (req,res)=>{
     try{
         const {workspace_id,role_name,color} = req.body;
-        const role_id = Role.findOne().sort({role_id:-1}).limit(1);
+        const role_id = await Role.findOne().sort({role_id:-1}).limit(1);
         await Role.create({role_id:role_id===null?0:role_id.role_id+1,workspace_id,role_name,color});
-        return res.json({ role_id, massage: "Login successfully!"});
+        return res.json({role_id, massage: "Login successfully!"});
     }
     catch(error){
         console.error(error);
